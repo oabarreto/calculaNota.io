7f8319edcaafa4a2b005661859c7d1a724adc272
Otávio Barreto <90889081+oabarreto@users.noreply.github.com>
2022-02-07 23:11:02 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,8 +6,24 @@ const avToggle = document.querySelector(".av3");
 
 const disabled = document.querySelector("#av3");
 
+let noteOne = document.querySelector("#av1").valueAsNumber;
+let noteTwo = Number(document.querySelector("#av2").value);
+let noteThree = Number(document.querySelector("#av3").value);
+
+let apsOne = Number(document.querySelector("#aps1").value);
+let apsTwo = Number(document.querySelector("#aps2").value);
+
+const calcButton = document.querySelector(".button");
+
+let status = document.querySelector(".status");
+let grades = document.querySelector(".grades");
+
 check.addEventListener("click", function () {
   avToggle.classList.toggle("on", off);
   disabled.toggleAttribute("disabled");
   off = !off;
 });
+
+function sum(noteOne, noteTwo, noteThree, apsOne, apsTwo) {}
+
+calcButton.addEventListener("click");
