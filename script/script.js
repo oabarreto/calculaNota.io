9d09f3708266ca9fa6a4f3742dee14f265de8dea
Otávio Barreto <90889081+oabarreto@users.noreply.github.com>
2022-02-08 22:32:22 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,10 +6,14 @@ const avToggle = document.querySelector(".av3");
 
 const disabled = document.querySelector("#av3");
 
-const calcButton = document.querySelector(".button");
+const calcButton = document.querySelector(".calc");
 
-let status = document.querySelector(".status");
-let grades = document.querySelector(".grades");
+const clearButton = document.querySelector(".clear");
+
+const resToggle = document.querySelector(".result");
+
+const statusClass = document.querySelector(".status");
+const grades = document.querySelector(".grades");
 
 check.addEventListener("click", function () {
   avToggle.classList.toggle("on", off);
@@ -24,18 +28,52 @@ calcButton.addEventListener("click", function () {
 
   let apsOne = document.querySelector("#aps1").valueAsNumber;
   let apsTwo = document.querySelector("#aps2").valueAsNumber;
-  let status = document.querySelector(".status");
-  let grades = document.querySelector(".grades");
 
-  const resToggle = document.querySelector(".result");
+  let res = 0;
 
-  resToggle.classList.toggle("on");
+  let attention = off === true && res < 7;
+  let approved = res > 7;
+  let disapproved = res < 7;
 
-  let res = (noteOne + noteTwo + apsOne + apsTwo) / 2;
+  let statusText;
+
+  resToggle.classList.add("on");
+
+  if (off === true) {
+    res = (noteOne + noteTwo + apsOne + apsTwo) / 2;
+  } else if (off === false) {
+    let totalAv1 = noteOne + apsOne;
+    let totalAv2 = noteTwo + apsTwo;
+    let min = Math.min(totalAv1, totalAv2, noteThree);
+    res = (totalAv1 + totalAv2 + noteThree - min) / 2;
+  }
 
   if (isNaN(res)) {
     res = "...";
   }
 
+  if (attention) {
+    statusClass.classList.toggle(".attention");
+    statusText = "av3";
+  } else if (approved) {
+    statusClass.classList.toggle(".approved");
+    statusText = "Aprovado";
+  } else if (disapproved) {
+    statusClass.classList.toggle(".disapproved");
+    statusText = "Reprovado";
+  }
+
+  statusClass.innerHTML = statusText;
   grades.innerHTML = res;
+});
+
+clearButton.addEventListener("click", function () {
+  resToggle.classList.remove("on");
+
+  let form = document.querySelector(".notes-form").reset();
+
+  let resetField = "...";
+
+  statusClass.innerHTML = resetField;
+  grades.innerHTML = resetField;
 });
